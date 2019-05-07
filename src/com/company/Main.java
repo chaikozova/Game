769d5600b71f9f5f0818514f7abf1dde16629819
chaikozova <r.chaikozova@gmail.com>
2019-05-07 09:37:57 +0600

    homework part.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        int health[] = {700, 250, 250, 250};
-        int hits[] = {50, 20, 20, 20};
-        String hitTypes[] = {"Physical", "Physical", "Magical", "Psyho"};
+        int health[] = {700, 250, 250, 250, 250};
+        int hits[] = {50, 20, 20, 20, 20};
+        String hitTypes[] = {"Physical", "Physical", "Magical", "Psyho", "Doctor"};
 
         fighting (health, hits, hitTypes);
 
@@ -25,6 +25,7 @@ public class Main {
         System.out.println("Thor health: "+health[1]);
         System.out.println("Capitan health: "+health[2]);
         System.out.println("Stark health: "+health[3]);
+        System.out.println("Doctor health: "+health[4]);
         System.out.println("________________");
     }
 
@@ -33,26 +34,37 @@ public class Main {
         if (!isNotFinish(health, false)) {
             return;
         }
-        BossBitsHeros(health, hits[0]);
-
+        if (health[4] != 0) {
+            for (int i = 1; i < 4; i++) {
+                health[i] = health [i] + hits [4];
+            }
+        }
+        BossBitsHeros(health, hits);
         changeBossHitTypes(hitTypes);
     }
 
     private static void changeBossHitTypes(String[] hitTypes) {
         Random random = new Random();
-        int randomInt = random.nextInt(3) + 1;
+        int randomInt = random.nextInt(4) + 1;
         hitTypes [0] = hitTypes [randomInt];
     }
 
     private static void HerosBitBoss(int[] health, int[] hits, String[] hitTypes) {
         for (int i = 1; i < 4; i++) {
-            health[0] = playerHit(hitTypes, i, hits, health );
+            if (health[0] != 0) {
+                health[0] = playerHit(hitTypes, i, hits, health);
+            }
         }
     }
 
-    private static void BossBitsHeros(int[] health, int hit) {
-        for (int i = 1; i < 4; i++) {
-            health[i] = health [i] - hit;
+    private static void BossBitsHeros(int[] health, int[] hits) {
+        for (int i = 1; i < 5; i++) {
+           if (health[i] != 0) {
+               health[i] = health[i] - hits[0];
+               if (hits[0] > health [i]) {
+                   health[i] = 0;
+               }
+           }
         }
     }
 
@@ -65,8 +77,11 @@ public class Main {
         } else {
             playerHit = hits[playerNumber];
         }
-
-        return health[0] - playerHit;
+        if (health[0] > playerHit) {
+            return health[0] - playerHit;
+        }  else {
+            return health[0] - health[0];
+        }
     }
 
     private static boolean isNotFinish(int health [], boolean showWin) {
